4b24cfe8aeaa069086e839d1072101d0f3787298
Tim Ruffles <oi@truffles.me.uk>
2015-08-20 14:42:05 +0100

    can't parse some yaml
    - pretty cool to see how easy it is to compose up fns etc, intuative
    - YAML library seems sucky
    - nice to be able to make `tapp` so easily, drop it in
    - ahhhhh... strict evaluation
    - for some reason my exception capturing thing isn't working

---
+++ f-sharp-language/Main.fs
@@ -0,0 +1,24 @@
+﻿open System.IO;
+open Yaml;
+open System.Text.RegularExpressions;
+
+type Post = { title: string }
+
+let unix2WindowsNl s = Regex.Replace(s, "\n", "\r\n")
+
+let tapp x =
+    (Regex.Escape >> printf "%s") x
+    x
+
+let getPosts directory = 
+    Directory.GetFiles(directory, "*.txt")
+    |> Seq.map (File.ReadAllText >> unix2WindowsNl >> tapp >> Yaml.load<Post>)
+
+
+
+try
+    let posts = getPosts(__SOURCE_DIRECTORY__ + "/posts")
+    printf "%A" posts
+with _ as ex ->
+    printf "failed to load posts"
+

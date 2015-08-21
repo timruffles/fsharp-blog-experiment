2aec74d316a08cb8598a9478b403c4494617dd94
Tim Ruffles <oi@truffles.me.uk>
2015-08-21 14:47:28 +0100

    parsing posts
    - gave up on fyaml, not sure why it wasn't happy
    - used fparsec instead, which looks great, and successfully parsed
      metadata and body of post
    - next steps: parse markdown (with a library, thank god)

--- f-sharp-language/Main.fs
+++ f-sharp-language/Main.fs
@@ -1,24 +1,41 @@
 ﻿open System.IO;
-open Yaml;
+open FParsec.CharParsers;
+open FParsec.Primitives;
 open System.Text.RegularExpressions;
+open FSharp.Formatting;
 
 type Post = { title: string }
 
-let unix2WindowsNl s = Regex.Replace(s, "\n", "\r\n")
-
 let tapp x =
-    (Regex.Escape >> printf "%s") x
+    (Regex.Escape >> printf "'%s'\n") x
     x
+
+let metadataPair = 
+  (regex "[a-z]+") .>>. ((regex ":\s*") >>. regex "[^\n]+") .>> skipNewline
+
+
+let postParser =
+    many metadataPair .>>. (manyCharsTill anyChar eof)
+
+ 
+
+let parsePost s =
+    match run postParser s with
+        | Success(result, _, _) -> printfn "%A" result
+        | Failure(errorMsg, _, _) -> printfn "%s" errorMsg
+
 
 let getPosts directory = 
     Directory.GetFiles(directory, "*.txt")
-    |> Seq.map (File.ReadAllText >> unix2WindowsNl >> tapp >> Yaml.load<Post>)
+    |> Seq.map (File.ReadAllText >> tapp >> parsePost)
 
 
 
 try
     let posts = getPosts(__SOURCE_DIRECTORY__ + "/posts")
     printf "%A" posts
-with _ as ex ->
+with | _ as ex ->
     printf "failed to load posts"
+
+
 

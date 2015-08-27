77d7986cd2503dd43a464a51efde4e7d5a85bd5a
Tim Ruffles <oi@truffles.me.uk>
2015-08-27 14:45:36 +0100

    posts parsed, and http server
    - using option and .find to create post records
    - removing invalid posts
    - copy/paste nanacy code ot get a web server
    
    next:
    
    - work out how to get state into web server

--- f-sharp-language/Main.fs
+++ f-sharp-language/Main.fs
@@ -2,9 +2,13 @@
 open FParsec.CharParsers;
 open FParsec.Primitives;
 open System.Text.RegularExpressions;
-open FSharp.Formatting;
 
-type Post = { title: string }
+
+type Post = { 
+    title: string
+    body: string
+}
+
 
 let tapp x =
     (Regex.Escape >> printf "'%s'\n") x
@@ -17,25 +21,59 @@ let metadataPair =
 let postParser =
     many metadataPair .>>. (manyCharsTill anyChar eof)
 
- 
+let markdown2html s =
+    let md = new MarkdownSharp.Markdown()
+    md.Transform(s)
+
+let createPostWithMeta (xs) =
+    let map = Map.ofSeq xs
+    let title = Map.find "title" map
+    { 
+        title = title
+        body = "" 
+    }
+
 
 let parsePost s =
     match run postParser s with
-        | Success(result, _, _) -> printfn "%A" result
-        | Failure(errorMsg, _, _) -> printfn "%s" errorMsg
+        | Success((meta, markdownBody), _,  _) ->
+            let post = createPostWithMeta meta
+            Some { post with body = markdown2html(markdownBody) }
+        | Failure(errorMsg, _, _) ->
+            printfn "%s" errorMsg
+            None
 
+let file2post = File.ReadAllText >> tapp >> parsePost
 
 let getPosts directory = 
     Directory.GetFiles(directory, "*.txt")
-    |> Seq.map (File.ReadAllText >> tapp >> parsePost)
+    |> Seq.map file2post
+    // remove invalid posts
+    |> Seq.choose id
+ 
+
+type Blog() as this =
+    inherit Nancy.NancyModule()
+    do
+        this.Get.["/"] <- fun _ -> "Hi there" :> obj
 
 
 
-try
-    let posts = getPosts(__SOURCE_DIRECTORY__ + "/posts")
-    printf "%A" posts
-with | _ as ex ->
-    printf "failed to load posts"
+[<EntryPoint>]
+let main args =
+
+    try
+        let posts = getPosts(__SOURCE_DIRECTORY__ + "/posts")
+        printf "%A" posts
+    with | _ as ex ->
+        printf "failed to load posts"
+
+ 
+    let nancy = new Nancy.Hosting.Self.NancyHost(new System.Uri("http://localhost:" + "8100"))
+    nancy.Start()
+    while true do System.Console.ReadLine() |> ignore
+    0
+
 
 
 
